8ad1f2d8d48e4296ee1fb717f92e392231ea2450
rubashkin_mikhail@tut.by <pristijpribor@mail.ru>
2019-12-04 08:40:53 +0300

    Initial commit

---
+++ src/main/java/com/example/roadcamera/RoadcameraApplication.java
@@ -0,0 +1,13 @@
+package com.example.roadcamera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadcameraApplication {
+
+    public static void main ( String[] args ){
+        SpringApplication.run (RoadcameraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/roadcamera/RoadcameraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.roadcamera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadcameraApplicationTests {
+
+    @Test
+    void contextLoads (){
+    }
+
+}
